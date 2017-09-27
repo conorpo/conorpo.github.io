324f1e7156ff98e79b523e0fdd422b544efa0725
conorpo <conor4arms@gmail.com>
2017-09-27 17:16:13 -0400

    Adds the balls

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 var value = 0;
+var context;
+var x=100;
+var y=200;
+var dx=5;
+var dy=5;
 function addValue(){
   value = value + 1;
   document.getElementById("value").innerHTML = value;
-};
+}
+function init()
+{
+  context= myCanvas.getContext('2d');
+  setInterval(draw,10);
+}
+
+function draw()
+{
+  context.clearRect(0,0, 300,300);
+  context.beginPath();
+  context.fillStyle="#0000ff";
+  // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
+  context.arc(x,y,20,0,Math.PI*2,true);
+  context.closePath();
+  context.fill();
+  // Boundary Logic
+if( x<0 || x>300) dx=-dx;
+if( y<0 || y>300) dy=-dy;
+x+=dx;
+y+=dy;
+}
