4c1a048c0b54e25f43751c5cae7caa31b1f5e146
conorpo <conor4arms@gmail.com>
2017-09-27 09:04:11 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let d = new Date();
-var x = document.createElement("P");
+var x = document.createElement("h1");
 var t = document.createTextNode("Todays date is " + d + " and hey Nick!");
 x.appendChild(t);
 document.body.appendChild(x);
