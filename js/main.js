f03d839ba70e7a608af546c7b465c452c58eb1c8
conorpo <conor4arms@gmail.com>
2017-09-28 08:07:20 -0400

    Added cursors

--- js/main.js
+++ js/main.js
@@ -7,19 +7,19 @@ var dy=3;
 function addValue(){
   value = value + 1;
   document.getElementById("value").innerHTML = value;
-  if(dx<0){
-    dx = -3 * (1+(0.001*value));
-  }
-  else{
-    dx = 3 * (1+(0.001*value));
-  }
-  if(dy<0){
-    dy = -3 * (1+(0.001*value));
-  }
-  else{
-    dy = 3 * (1+(0.001*value));
-  }
-}
+var cursors = 0;
+
+function buyCursor(){
+    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
+    if(value >= cursorCost){                                   //checks that the player can afford the cursor
+        cursors = cursors + 1;                                   //increases number of cursors
+    	  value = value - cursorCost;                          //removes the cookies spent
+        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
+        document.getElementById('value').innerHTML = value;  //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
+    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+};
 function init()
 {
   context= myCanvas.getContext('2d');
@@ -41,3 +41,21 @@ if( y<=20 || y>=380) dy=-dy;
 x+=dx;
 y+=dy;
 }
+window.setInterval(function(){
+	
+	value = value + cursors;
+  if(dx<0){
+    dx = -3 * (1+(0.001*value));
+  }
+  else{
+    dx = 3 * (1+(0.001*value));
+  }
+  if(dy<0){
+    dy = -3 * (1+(0.001*value));
+  }
+  else{
+    dy = 3 * (1+(0.001*value));
+  }
+}
+	
+}, 1000);
