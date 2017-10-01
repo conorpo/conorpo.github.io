345ecd1d791161f84d3262eec6be7aab0075e050
conorpo <conor4arms@gmail.com>
2017-10-01 19:55:19 -0400

    Added multiple balls

--- js/main.js
+++ js/main.js
@@ -1,24 +1,18 @@
 var value = 0;
 var context;
-var x=100;
+var balls = [
+  {active:true,x:200,y:200,dx:3,dy:3,color:"#0000ff"},
+  {active:true,x:100,y:100,dx:3,dy:3,color:"#ff0000"},
+  {active:true,x:300,y:300,dx:3,dy:3,color:"#00ff00"},
+  {active:false,x:400,y:400,dx:3,dy:3,color:"#ffff00"}
+]
 var y=200;
 var dx=3;
 var dy=3;
-function addValue(){
+function addValue()
+{
   value = value + 1;
   document.getElementById("value").innerHTML = value;
-  if(dx<0){
-    dx = -3 * (1+(0.001*value));
-  }
-  else{
-    dx = 3 * (1+(0.001*value));
-  }
-  if(dy<0){
-    dy = -3 * (1+(0.001*value));
-  }
-  else{
-    dy = 3 * (1+(0.001*value));
-  }	
 }
 function init()
 {
@@ -28,16 +22,16 @@ function init()
 
 function draw()
 {
-  context.clearRect(0,0, 500,400);
-  context.beginPath();
-  context.fillStyle="#0000ff";
-  // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
-  context.arc(x,y,20,0,Math.PI*2,true);
-  context.closePath();
-  context.fill();
-  // Boundary Logic
-if( x<=20 || x>=480) dx=-dx;
-if( y<=20 || y>=380) dy=-dy;
-x+=dx;
-y+=dy;
+  context.clearRect(0,0, 500,500);
+  for(var ball in balls){
+    context.beginPath();
+      context.fillStyle=ball.color;
+    context.arc(ball.x,ball.y,20,0,Math.PI*2,true);
+    context.closePath();
+    context.fill();
+    if(ball.x<=20 || ball.x>=480) ball.dx=-ball.dx;
+    if(ball.y<=20 || ball.y>=480) ball.dy=-ball.dy;
+    ball.x+=ball.dx;
+    ball.y+=ball.dy;
+  }
 }
