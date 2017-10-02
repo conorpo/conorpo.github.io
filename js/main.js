92ee511b1cde80d0a7c79c10ba43a5e9100da646
conorpo <conor4arms@gmail.com>
2017-10-01 20:11:05 -0400

    Added support for different radii

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ var height = 700;
 var value = 0;
 var context;
 var balls = [
-  {active:true,x:200,y:200,dx:3,dy:3,color:"#0000ff"},
-  {active:true,x:100,y:100,dx:-3,dy:3,color:"#ff0000"},
-  {active:true,x:300,y:300,dx:3,dy:-3,color:"#00ff00"},
-  {active:false,x:400,y:400,dx:-3,dy:-3,color:"#ffff00"}
+  {active:true,x:200,y:200,dx:3,dy:3,color:"#0000ff",radius:20},
+  {active:true,x:100,y:100,dx:-3,dy:3,color:"#ff0000",radius:10},
+  {active:true,x:300,y:300,dx:3,dy:-3,color:"#00ff00",radius:30},
+  {active:false,x:400,y:400,dx:-3,dy:-3,color:"#ffff00",radius:15}
 ]
 var y=200;
 var dx=3;
@@ -31,11 +31,11 @@ function draw()
     {
       context.beginPath();
       context.fillStyle=ball.color;
-      context.arc(ball.x,ball.y,20,0,Math.PI*2,true);
+      context.arc(ball.x,ball.y,ball.radius,0,Math.PI*2,true);
       context.closePath();
       context.fill();
-      if(ball.x<=20 || ball.x>=(width-20)) ball.dx=-ball.dx;
-      if(ball.y<=20 || ball.y>=(height-20)) ball.dy=-ball.dy;
+      if(ball.x<=ball.radius || ball.x>=(width-ball.radius)) ball.dx=-ball.dx;
+      if(ball.y<=ball.radius || ball.y>=(height-ball.radius)) ball.dy=-ball.dy;
       ball.x+=ball.dx;
       ball.y+=ball.dy;
     }
