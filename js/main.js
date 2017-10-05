97a1198225d4306e43c7046daa277dbfc1d9471f
Unknown <conor4arms@gmail.com>
2017-10-05 12:25:53 -0400

    Added exponential health

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var width = 1100;
 var height = 700;
 var value = 0;
 var context;
-var level = 1; 
+var level = 1;
 var balls = [
   {active:true,x:200,y:200,dx:.707,dy:.707,color:"#0000ff",radius:20,damage:8},
   {active:true,x:100,y:100,dx:-.707,dy:.707,color:"#ff0000",radius:10,damage:20},
@@ -24,7 +24,7 @@ function newLevel() {
   document.getElementById("level").innerHTML = level;
   enemies = [];
   for(i = 0;i<5;i++){
-    var healthTemp = Math.floor(Math.pow(rand(100,200),level/2));
+    var healthTemp = 100*Math.pow(1.07,level-1);
     enemies.push({x:rand(100, 1000),y:rand(100, 600),health:healthTemp,sHealth:healthTemp,active:true,radius:rand(20,60)});
   }
 }
@@ -54,8 +54,8 @@ function draw(){
       context.closePath();
       context.fill();
       context.font = Math.floor(enemyDraw.radius/1.7)+"px Arial";
-      context.textAlign="center"; 
-      context.textBaseline="middle"; 
+      context.textAlign="center";
+      context.textBaseline="middle";
       context.fillStyle="#FFFFFF";
       context.fillText(Math.ceil((enemyDraw.health/enemyDraw.sHealth)*100)+"%",enemyDraw.x,enemyDraw.y);
     }
@@ -72,14 +72,14 @@ function draw(){
         var enemy = enemies[e];
         if(enemy.active){
           if(Math.pow(ball.x-enemy.x,2)+Math.pow(ball.y-enemy.y,2)<=Math.pow(enemy.radius+ball.radius,2)){
-            if(enemy.health>ball.damage)  
+            if(enemy.health>ball.damage)
               {
                 enemy.health -= ball.damage;
-              } 
+              }
             else
               {
                 enemy.active = false;
-                addValue(enemy.sHealth);
+                addValue(100*Math.Pow(1.02,level-1));
                 levelCheck();
               }
             ball.dx = (ball.x - enemy.x)/Math.sqrt(Math.pow(ball.x-enemy.x,2)+Math.pow(ball.y-enemy.y,2));
