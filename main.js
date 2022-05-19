5ae1f02f2a326c1692ea0277b1a771745c31190c
Conor O'Malley <conor4arms@gmail.com>
2022-05-19 18:22:54 -0400

    Basic Cards

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
-var x = window.matchMedia("(max-width: 810px)");
-function init(){
-  var main = document.getElementsByClassName("project");
-  for(var i = 0; i<main.length;i++){
-    var child = main[i];
-    var height = child.children[0].height;
-    if(!x.matches){
-        child.children[1].style.lineHeight = height + "px";
-        child.children[1].style.fontSize = height/2+ "px";
-    }
-    else{
-      child.children[1].style.fontSize = "7vw";
-      child.children[1].style.lineHeight = "7vw";
-    }
-    //child.children[2].style.margin = (height/4)-2+"px"+" 0";
-  }
+function makeCard(name){
+  const cardLayer = document.getElementById("card-layer");
+
+  const element = document.createElement("div");
+  element.classList.add("card");
+  element.innerText = name;
+  element.style.bottom = "-" + (config.cardHandRadius + 2) + "px";
+  cardLayer.appendChild(element);
+
+  state.cards.push({name, element});
 }
+
+const config = {
+  cardHandAngle: 30,
+  cardHandRadius: 500,
+};
+
+const state = {
+  mouse: {x: 0, y: 0},
+  cards: []
+}
+
+function getTransformString(angle){
+  return `rotate(${angle}deg) translateY(-${config.cardHandRadius}px)`;
+}
+
+function init(){
+  config.startingAngle = ((config.cardHandAngle/2 + 90)/360)*(Math.PI * 2);
+
+  makeCard("Info");
+  makeCard("Projects")
+  makeCard("School");
+
+  updateCardPositions();
+}
+
+function updateCardPositions(){
+  const angleDif= config.cardHandAngle/(state.cards.length-1);
+  let currentAngle = -config.cardHandAngle/2;
+  state.cards.forEach((card, i) => {
+    state.cards[i].element.style.transform = getTransformString(currentAngle);
+    currentAngle+=angleDif;
+  })
+}
+
+setInterval(updateCardPositions, 10);
