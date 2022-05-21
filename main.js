c58174b869f07d089964ca28e3784061fa5432dc
Conor O'Malley <conor4arms@gmail.com>
2022-05-21 00:18:02 -0400

    Day 2 Of Work

--- main.js
+++ main.js
@@ -1,46 +1,146 @@
-function makeCard(name){
-  const cardLayer = document.getElementById("card-layer");
+/*
+  TODO
 
-  const element = document.createElement("div");
-  element.classList.add("card");
-  element.innerText = name;
-  element.style.bottom = "-" + (config.cardHandRadius + 2) + "px";
-  cardLayer.appendChild(element);
+  Make Card Hand System More Dynamic (swap around cards, place cards in spots that make sense)
 
-  state.cards.push({name, element});
-}
+  REDO Z-Index Event Bubling So Its Not Shit
+
+  Make Card Placement Dynamic
+
+  Clean Up Code Overall
+*/
 
 const config = {
+  updateInterval: 20,
+  cardDealSpeed: 8,
+
   cardHandAngle: 30,
   cardHandRadius: 500,
+  cardWidth: 120,
+  cardHeight: 200,
+
+  wobbleIntensity: 10,
+  wobbleSpeed: 3
 };
+
+const elements = {
+  globalContainer: null,
+  playingArea: null
+}
 
 const state = {
   mouse: {x: 0, y: 0},
-  cards: []
+  cards: [],
+  draggedCardIndex: -1,
+  activeCardIndex: -1,
+  cardQueue: []
 }
 
-function getTransformString(angle){
-  return `rotate(${angle}deg) translateY(-${config.cardHandRadius}px)`;
+function getTransformStringHand(angle){
+  return `translate(${window.innerWidth/2}px, ${window.innerHeight + config.cardHandRadius - config.cardHeight}px) rotate(${angle}deg) translateY(-${config.cardHandRadius}px)`;
+}
+
+function getTransformStringDrag(){
+  const angle = (frame*config.wobbleSpeed/1000)%(2*Math.PI);
+  return `translate(${state.mouse.x - config.cardWidth/2}px, ${state.mouse.y - config.cardHeight/2}px) scale(1.1)  rotate3d(${Math.sin(angle)},${Math.cos(angle)},0, ${config.wobbleIntensity}deg)`;
+}
+
+function getRandomTransform(){
+  return `rotate(${Math.random()*60 + -30}deg) scale(${Math.random()*3 + 0.3})`;
 }
 
 function init(){
-  config.startingAngle = ((config.cardHandAngle/2 + 90)/360)*(Math.PI * 2);
+  elements.globalContainer = document.getElementById("global-container");
+  elements.playingArea = document.getElementById("playing-area");
 
-  makeCard("Info");
-  makeCard("Projects")
-  makeCard("School");
+  elements.playingArea.addEventListener("mouseup", evt => {
+    console.log("A");
+    if(state.activeCardIndex != -1){
+      state.cards[state.activeCardIndex].infoElement.classList.add("hidden");
+      state.cards[state.activeCardIndex].infoElement.style.transform = getRandomTransform();
+    }
 
-  updateCardPositions();
-}
+    if(state.draggedCardIndex != -1){
+      state.cards[state.draggedCardIndex].element.classList.remove("drag");
+      state.cards[state.draggedCardIndex].element.style.transform = "translate(7.55vw, 30vh)"
+      state.activeCardIndex = state.draggedCardIndex;
+      state.draggedCardIndex = -1;  
+      state.cards[state.activeCardIndex].infoElement.classList.remove("hidden");
+      state.cards[state.activeCardIndex].infoElement.style.transform = "";
 
-function updateCardPositions(){
-  const angleDif= config.cardHandAngle/(state.cards.length-1);
-  let currentAngle = -config.cardHandAngle/2;
-  state.cards.forEach((card, i) => {
-    state.cards[i].element.style.transform = getTransformString(currentAngle);
-    currentAngle+=angleDif;
+    }
+  });
+
+  elements.globalContainer.addEventListener("mouseup", evt=> {
+    console.log("B");
+    if(state.draggedCardIndex != -1){
+      state.cards[state.draggedCardIndex].element.classList.remove("drag");
+      state.draggedCardIndex = -1;
+    }
+    elements.globalContainer.style.cursor = "auto";
+  });
+
+  elements.globalContainer.addEventListener("mousemove", evt => {
+    state.mouse.x = evt.clientX;
+    state.mouse.y = evt.clientY;
   })
+
+  state.cardQueue = ["about", "projects", "school"];
+  setInterval(updateCardPositions, config.updateInterval);
 }
 
-setInterval(updateCardPositions, 10);
+function makeCard(name){
+  const index = state.cards.length;
+
+  //Basic
+  const element = document.createElement("div");
+  const infoElement = document.getElementsByClassName(name)[0];
+  infoElement.style.transform = getRandomTransform();
+  element.classList.add("card");
+  element.innerText = name;
+  element.style.height = config.cardHeight + "px";
+  element.style.width = config.cardWidth + "px";
+  element.style.bottom = "-" + (config.cardHandRadius + 2) + "px";
+  
+  //Dragging
+  element.addEventListener("mousedown", evt => {
+    state.draggedCardIndex = index;
+    evt.target.classList.add("drag");
+    evt.target.transform = getTransformStringDrag();
+    elements.globalContainer.style.cursor = "pointer";
+
+    if(index == state.activeCardIndex){
+      state.cards[state.activeCardIndex].infoElement.style.transform = getRandomTransform();
+      state.activeCardIndex = -1;
+      state.cards[index].infoElement.classList.add("hidden");
+    }
+  })
+  
+  elements.globalContainer.appendChild(element);
+  state.cards.push({name, element, infoElement});
+}
+
+let frame = 0;
+function updateCardPositions(){
+  frame += config.updateInterval;
+
+  //Dragged Card
+  if(state.draggedCardIndex != -1) {
+    state.cards[state.draggedCardIndex].element.style.transform = getTransformStringDrag();
+  }
+
+  //Cards in Hand
+  const amountOfCards = state.cards.length - (state.draggedCardIndex != -1) - (state.activeCardIndex != -1) - 1;
+  const angleDif = (amountOfCards) ? config.cardHandAngle/amountOfCards : 0;
+  let currentAngle = (amountOfCards) ? -config.cardHandAngle/2 : 0;
+  for(let i = 0; i < state.cards.length; i++){
+    if(i == state.draggedCardIndex || i == state.activeCardIndex) continue;
+    state.cards[i].element.style.transform = getTransformStringHand(currentAngle);
+    currentAngle+=angleDif;
+  }
+
+  if(state.cardQueue.length && frame%(config.updateInterval*config.cardDealSpeed) == 0){
+    makeCard(state.cardQueue[0]);
+    state.cardQueue.shift();
+  }
+}
